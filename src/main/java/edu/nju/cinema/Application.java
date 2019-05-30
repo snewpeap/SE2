d23b6e2ae5194318295d76b47a2a2fe0ea56ea44
YRD <1127199932@qq.com>
2019-05-30 11:58:18 +0800

    configuration dependencies by YRD @ 2019-5-30 11:58:04

---
+++ src/main/java/edu/nju/cinema/Application.java
@@ -0,0 +1,10 @@
+package edu.nju.cinema;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
