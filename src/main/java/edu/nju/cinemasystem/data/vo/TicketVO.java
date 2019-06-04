eb29e315f6205abcc6433b3e9f2378b615b9f308
171250524 <171250524@smail.nju.edu.cn>
2019-06-04 20:54:55 +0800

    增加sale,promotion,statistics的接口 by 赖宝光

---
+++ src/main/java/edu/nju/cinemasystem/data/vo/TicketVO.java
@@ -0,0 +1,77 @@
+package edu.nju.cinemasystem.data.vo;
+
+import java.util.Date;
+
+public class TicketVO {
+    
+    private Integer id;
+
+    private Integer userId;
+
+    private Integer arrangementId;
+
+    private Integer seatId;
+
+    private Date date;
+
+    private Byte status;
+
+    private Float realAmount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getArrangementId() {
+        return arrangementId;
+    }
+
+    public void setArrangementId(Integer arrangementId) {
+        this.arrangementId = arrangementId;
+    }
+
+    public Integer getSeatId() {
+        return seatId;
+    }
+
+    public void setSeatId(Integer seatId) {
+        this.seatId = seatId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    public Float getRealAmount() {
+        return realAmount;
+    }
+
+    public void setRealAmount(Float realAmount) {
+        this.realAmount = realAmount;
+    }
+    
+}
