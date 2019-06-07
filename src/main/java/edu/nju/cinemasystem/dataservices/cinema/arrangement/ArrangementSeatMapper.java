5796cbe2e1acf9bf5d2975e89358491cc42bb66a
YRD <1127199932@qq.com>
2019-06-07 22:29:46 +0800

    Arrangement部分的数据层实现和注释都写了哦 by YRD @ 2019年6月7日22:29:38

--- src/main/java/edu/nju/cinemasystem/dataservices/cinema/arrangement/ArrangementSeatMapper.java
+++ src/main/java/edu/nju/cinemasystem/dataservices/cinema/arrangement/ArrangementSeatMapper.java
@@ -9,19 +9,45 @@ import java.util.List;
 @Mapper
 @Repository
 public interface ArrangementSeatMapper {
+    /**
+     * 插入一条记录
+     * @param record ArrangementSeatPO
+     * @return 调用者应拥有id，所以返回操作的行数
+     */
     int insert(ArrangementSeat record);
 
+    /**
+     * 插入一条记录，为null的属性不会被写进sql语句中
+     * @param record ArrangementSeatPO
+     * @return 调用者应拥有id，所以返回操作的行数
+     */
     int insertSelective(ArrangementSeat record);
 
-    //TODO
+    /**
+     * 更新一条记录，两个id都必须有
+     * @param record ArrangementSeatPO
+     * @return 调用者应拥有id，所以返回操作的行数
+     */
     int updateSeatStatus(ArrangementSeat record);
 
-    //TODO
+    /**
+     * 虽然说用途是删除一条记录
+     * @param record 包含seatId和arrangementId的PO
+     * @return 操作的记录条数，0为没有进行任何操作
+     */
     int delete(ArrangementSeat record);
 
-    //TODO
-    List<ArrangementSeat> selectByArrangementID(int ArrangementID);
+    /**
+     * 通过排片ID批量删除记录
+     * @param arrangementId 排片id
+     * @return 此排片id对应的所有记录
+     */
+    List<ArrangementSeat> selectByArrangementID(int arrangementId);
 
-    //TODO
+    /**
+     * 查找一条记录
+     * @param record ArrangementSeatPO
+     * @return 找到的PO
+     */
     ArrangementSeat select(ArrangementSeat record);
 }
