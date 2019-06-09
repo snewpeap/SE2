1f7f282d0ef0300b41b694f67ca75540cae28c50
YRD <1127199932@qq.com>
2019-06-09 22:02:15 +0800

    SpringSecurity尝试 by YRD @ 2019年6月9日18:07:46

--- src/main/java/edu/nju/cinemasystem/util/security/GlobalUserDetailService.java
+++ src/main/java/edu/nju/cinemasystem/util/security/GlobalUserDetailService.java
@@ -1,8 +1,10 @@
 package edu.nju.cinemasystem.util.security;
 
 import edu.nju.cinemasystem.data.po.User;
+import edu.nju.cinemasystem.dataservices.user.RoleMapper;
 import edu.nju.cinemasystem.dataservices.user.UserHasRoleMapper;
 import edu.nju.cinemasystem.dataservices.user.UserMapper;
+import edu.nju.cinemasystem.util.properties.RoleProperty;
 import edu.nju.cinemasystem.util.properties.message.AccountMsg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -18,11 +20,15 @@ import java.util.List;
 public class GlobalUserDetailService implements UserDetailsService {
 
     @Autowired
+    private RoleMapper roleMapper;
+    @Autowired
     private UserMapper userMapper;
     @Autowired
     private UserHasRoleMapper userHasRoleMapper;
     @Autowired
     private AccountMsg accountMsg;
+    @Autowired
+    private RoleProperty roleProperty;
 
     @Override
     public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
@@ -30,7 +36,26 @@ public class GlobalUserDetailService implements UserDetailsService {
         if (user == null){
             throw new UsernameNotFoundException(accountMsg.getAccountNotExist());
         }
-        List<SimpleGrantedAuthority> auths = new ArrayList<>(3);
-        return null;
+        List<SimpleGrantedAuthority> auth = new ArrayList<>(3);
+        String role = roleMapper.selectByPrimaryKey(userHasRoleMapper.selectByUserID(user.getId()).getRoleId()).getRoleName();
+        SimpleGrantedAuthority staffAuth = new SimpleGrantedAuthority(roleProperty.getStaff());
+        SimpleGrantedAuthority managerAuth = new SimpleGrantedAuthority(roleProperty.getManager());
+        if (roleProperty.getRoot().equals(role)) {
+            auth.add(new SimpleGrantedAuthority(roleProperty.getRoot()));
+            auth.add(managerAuth);
+            auth.add(staffAuth);
+        }else if (roleProperty.getManager().equals(role)){
+            auth.add(managerAuth);
+            auth.add(staffAuth);
+        }else if (roleProperty.getStaff().equals(role)){
+            auth.add(staffAuth);
+        }else {
+            auth.add(new SimpleGrantedAuthority(roleProperty.getAudience()));
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getName(),
+                user.getPassword(),
+                auth
+                );
     }
 }
--- src/main/java/edu/nju/cinemasystem/web/config/CustomWebSecurityConfiguration.java
+++ src/main/java/edu/nju/cinemasystem/web/config/CustomWebSecurityConfiguration.java
@@ -1,7 +1,8 @@
 package edu.nju.cinemasystem.web.config;
 
+import edu.nju.cinemasystem.util.properties.RoleProperty;
 import edu.nju.cinemasystem.util.security.GlobalUserDetailService;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,19 +13,38 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class CustomWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    GlobalUserDetailService globalUserDetailService(){
-        return new GlobalUserDetailService();
+    private final RoleProperty roleProperty;
+    private GlobalUserDetailService globalUserDetailService;
+
+    @Autowired
+    public CustomWebSecurityConfiguration(RoleProperty roleProperty, GlobalUserDetailService globalUserDetailService) {
+        this.roleProperty = roleProperty;
+        this.globalUserDetailService = globalUserDetailService;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(globalUserDetailService());
+        auth.userDetailsService(globalUserDetailService);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //TODO
-        super.configure(http);
+        http.authorizeRequests()
+                .antMatchers("/index","/register","/logout").permitAll()
+                .antMatchers("/manage/**").hasAnyRole(
+                        roleProperty.getRoot(), roleProperty.getManager(),roleProperty.getRoot())
+                .antMatchers("/admin/**").hasAnyRole(
+                        roleProperty.getRoot(),roleProperty.getManager())
+                .antMatchers("/root/**").hasRole(roleProperty.getRoot())
+                .regexMatchers("^/(?!manage|admin|root).*$").hasRole(roleProperty.getAudience())
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/index")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .permitAll();
     }
 }
