742bcc90a172510968c245e71debef7efb589510
Weijin1455236662 <1455236662@qq.com>
2019-06-09 19:34:37 +0800

    好了

--- src/main/resources/static/js/user/jquery.magnific-popup.js
+++ src/main/resources/static/js/user/jquery.magnific-popup.js
@@ -506,7 +506,7 @@ MagnificPopup.prototype = {
 			if(markup) {
 				mfp.currTemplate[type] = $(markup);
 			} else {
-				// if there is no markup found we just define that template is parsed
+				// if there is no markup found we just define that templates is parsed
 				mfp.currTemplate[type] = true;
 			}
 		}
