148e674751c29010676caa44c1aba0023020ffb0
YRD <1127199932@qq.com>
2019-06-09 16:39:30 +0800

    暂存

--- src/main/java/edu/nju/cinemasystem/web/config/CustomWebSecurityConfiguration.java
+++ src/main/java/edu/nju/cinemasystem/web/config/CustomWebSecurityConfiguration.java
@@ -1,9 +1,30 @@
 package edu.nju.cinemasystem.web.config;
 
+import edu.nju.cinemasystem.util.security.GlobalUserDetailService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class CustomWebSecurityConfiguration {
+public class CustomWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    GlobalUserDetailService globalUserDetailService(){
+        return new GlobalUserDetailService();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(globalUserDetailService());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //TODO
+        super.configure(http);
+    }
 }
