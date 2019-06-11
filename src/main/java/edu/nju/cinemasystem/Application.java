37be7620dc639a250efe10b732ec859526755bb7
YRD <1127199932@qq.com>
2019-06-11 12:43:53 +0800

    tomcat config by YRD @ 2019年6月11日12:43:45

--- src/main/java/edu/nju/cinemasystem/Application.java
+++ src/main/java/edu/nju/cinemasystem/Application.java
@@ -3,14 +3,22 @@ package edu.nju.cinemasystem;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @MapperScan(basePackages = "edu.nju.cinemasystem.dataservices")
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
