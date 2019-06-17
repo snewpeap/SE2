689f24fab580a3df73d3f1d4d799b8988eec36df
171250524 <171250524@smail.nju.edu.cn>
2019-06-17 10:50:11 +0800

    加了获取所有退票策略的接口，把info的实现分开 by 赖宝光 @2019年6月17日10点49分

--- src/main/java/edu/nju/cinemasystem/blservices/impl/sale/ticket/RefundTicketManageImpl.java
+++ src/main/java/edu/nju/cinemasystem/blservices/impl/sale/ticket/RefundTicketManageImpl.java
@@ -4,6 +4,7 @@ import edu.nju.cinemasystem.blservices.sale.ticket.RefundTicketManage;
 import edu.nju.cinemasystem.data.po.RefundStrategy;
 import edu.nju.cinemasystem.data.vo.Response;
 import edu.nju.cinemasystem.data.vo.form.RefundStrategyForm;
+import edu.nju.cinemasystem.data.vo.statisticsVO.RefundStrategyVO;
 import edu.nju.cinemasystem.dataservices.sale.ticket.RefundStrategyMapper;
 import edu.nju.cinemasystem.util.properties.message.TicketMsg;
 import edu.nju.cinemasystem.util.properties.message.GlobalMsg;
@@ -11,6 +12,9 @@ import edu.nju.cinemasystem.util.properties.message.GlobalMsg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RefundTicketManageImpl implements RefundTicketManage {
@@ -57,6 +61,16 @@ public class RefundTicketManageImpl implements RefundTicketManage {
         refundStrategyMapper.updateByPrimaryKeySelective(RefundStrategy.assembleRefundStrategyPO(days, refundable, percentage));
         response = Response.success();
         response.setMessage(globalMsg.getOperationSuccess());
+        return response;
+    }
+
+    @Override
+    public Response getAllRefunds(){
+        List<RefundStrategy> refundStrategies = refundStrategyMapper.selectAll();
+        List<RefundStrategyVO> refundStrategyVOS = new ArrayList<>();
+        refundStrategies.forEach(refundStrategy -> refundStrategyVOS.add(new RefundStrategyVO(refundStrategy)));
+        Response response = Response.success();
+        response.setContent(refundStrategyVOS);
         return response;
     }
 }
