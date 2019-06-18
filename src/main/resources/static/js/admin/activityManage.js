921434c887196596c2fefca821c0f78142d8705c
Weijin1455236662 <1455236662@qq.com>
2019-06-19 04:02:28 +0800

    优惠活动管理优化 by 魏进

--- src/main/resources/static/js/admin/activityManage.js
+++ src/main/resources/static/js/admin/activityManage.js
@@ -21,40 +21,43 @@ $(document).ready(function() {
         var activitiesDomStr = "";
 
         activities.forEach(function (activity) {
-            var movieDomStr = "";
-            if(activity.movieList.length){
-                activity.movieList.forEach(function (movie) {
-                    movieDomStr += "<li class='activity-movie primary-text'>"+movie+"</li>";
-                });
-            }else{
-                movieDomStr = "<li class='activity-movie primary-text'>所有热映电影</li>";
-            }
+            if(new Date(activity.endTime).getTime() > new Date().getTime()){
+                var movieDomStr = "";
+                if(activity.movieList.length){
+                    activity.movieList.forEach(function (movie) {
+                        movieDomStr += "<li class='activity-movie primary-text'>"+movie+"</li>";
+                    });
+                }else{
+                    movieDomStr = "<li class='activity-movie primary-text'>所有热映电影</li>";
+                }
 
-            activitiesDomStr+=
-                "<div class='activity-container'>" +
-                    "<div class='activity-card card'>" +
-                        "<div class='activity-line'>" +
-                            "<span class='title'>"+activity.name+"</span>" +
+                activitiesDomStr+=
+                    "<div class='activity-container'>" +
+                        "<div class='activity-card card'>" +
+                            "<div class='activity-line'>" +
+                                "<span class='title'>"+activity.name+"</span>" +
+                            "</div>" +
+                            "<div class='activity-line'>" +
+                                "<span>活动描述："+activity.description+"</span>" +
+                            "</div>" +
+                            "<div class='activity-line'>" +
+                                "<span>活动时间："+formatDate(new Date(activity.startTime))+"至"+formatDate(new Date(activity.endTime))+"</span>" +
+                            "</div>" +
+                            "<div class='activity-line'>" +
+                                "<span>参与电影：</span>" +
+                                "<ul>"+movieDomStr+"</ul>" +
+                            "</div>" +
                         "</div>" +
-                        "<div class='activity-line'>" +
-                            "<span>活动描述："+activity.description+"</span>" +
+                        "<div class='activity-coupon primary-bg'>" +
+                            "<span class='title'>优惠券："+activity.name+"</span>" +
+                            "<span class='title'>满"+activity.targetAmount+"减<span class='error-text title'>"+activity.couponAmount+"</span></span>" +
+                            "<span class='gray-text'>"+activity.description+"</span>" +
                         "</div>" +
-                        "<div class='activity-line'>" +
-                            "<span>活动时间："+formatDate(new Date(activity.startTime))+"至"+formatDate(new Date(activity.endTime))+"</span>" +
-                        "</div>" +
-                        "<div class='activity-line'>" +
-                            "<span>参与电影：</span>" +
-                            "<ul>"+movieDomStr+"</ul>" +
-                        "</div>" +
-                    "</div>" +
-                    "<div class='activity-coupon primary-bg'>" +
-                        "<span class='title'>优惠券："+activity.name+"</span>" +
-                        "<span class='title'>满"+activity.targetAmount+"减<span class='error-text title'>"+activity.couponAmount+"</span></span>" +
-                        "<span class='gray-text'>"+activity.description+"</span>" +
-                    "</div>" +
-                "</div>";
+                    "</div>";
+            }
         });
         $(".content-activity").append(activitiesDomStr);
+
     }
 
     function getAllMovies() {
