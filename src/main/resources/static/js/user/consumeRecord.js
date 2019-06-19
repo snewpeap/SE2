5d493a6b33c998468193ca12c78552401054567b
Weijin1455236662 <1455236662@qq.com>
2019-06-19 11:52:57 +0800

    消费记录加了时间，pay三页面删除buy.js

--- src/main/resources/static/js/user/consumeRecord.js
+++ src/main/resources/static/js/user/consumeRecord.js
@@ -1,7 +1,6 @@
 var consumeRecord = [];
 var order;
 $(document).ready(function () {
-    let userID = getCookie('id');
     getRequest(
         '/user/purchaseRecord?userId=' + getCookie('id'),
         function (res) {
@@ -21,33 +20,21 @@ $(document).ready(function () {
     );
 
     function addRecord(record) {
-        let recordStr = '';
+        var recordStr = '';
         recordStr +=
             "<tr id='"+record.orderID+"' title='";
         record.ticketVOs.forEach(function (ticket) {
             recordStr += '电影名称：'+record.movieName + '          座位：' + ticket.row + '排' + ticket.column + '座' + '          电影票状态：' + ticket.status + '&#10;'
         });
         recordStr += "'>"+
-            "<td id='"+record.orderID+"' style=\"width: 115px\">"+ record.movieName+"</td>" +
-            "<td id='"+record.orderID+"' style=\"width: 195px\">"+ record.startTime.replace('T',' ').substring(0,19)+"</td>" +
-            "<td id='"+record.orderID+"' style=\"width: 80px\">"+ record.ticketVOs.length +"张</td>" +
-            "<td id='"+record.orderID+"' style='width: 120px'>" + record.originalSpend + "元</td>" +
-            "<td id='"+record.orderID+"' style='width: 120px'>" + (record.realSpend).toFixed(2) + "元</td>" +
+            "<td>"+ record.movieName+"</td>" +
+            "<td>"+ record.startTime.replace('T',' ').substring(0,19)+"</td>" +
+            "<td>"+ record.ticketVOs.length +"张</td>" +
+            "<td>" + record.originalSpend + "元</td>" +
+            "<td>" + (record.realSpend).toFixed(2) + "元</td>" +
+            "<td>"+record.completeTime.replace('T',' ').substring(0,19)+"</td>" +
             "</tr>" ;
         $(".table tbody").append(recordStr);
     }
     }
 );
-
-// function getTicketInfo(record) {
-//     let ticketStr = '';
-//     record.ticketVOs.forEach(function (ticketVO) {
-//             ticketStr +=
-//                 "电影名称："+ ticketVO.name +
-//                 "放映影厅："+ ticketVO.hallname +
-//                 "座位：" + (ticketVO.row) + "排" + (ticketVO.column) + "座" +
-//                 "开始时间：" + ticketVO.startDate +
-//                 "结束时间：" + ticketVO.endDate;
-//         }
-//     )
-// }
