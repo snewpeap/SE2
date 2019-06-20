7ecdff82a3dccb6350d8da0c62dadceee033650e
171250026@smail.nju.edu.cn <171250026@smail.nju.edu.cn>
2019-06-20 11:38:03 +0800

    MovieVO by Ava

--- src/main/java/edu/nju/cinemasystem/data/vo/AudienceMovieVO.java
+++ src/main/java/edu/nju/cinemasystem/data/vo/AudienceMovieVO.java
@@ -2,6 +2,14 @@ package edu.nju.cinemasystem.data.vo;
 
 import java.util.List;
 
+/**
+ * 给观众的电影信息 继承自 BaseMovieVO 包含：
+ * status 状态：0-未上映 1-热映中 2-已下映 3-已下架
+ * joinedPromotions 加入的活动
+ * likeNum 想看人数
+ * isLiked 这个观众是否想看了这个电影
+ * heat 热度
+ */
 public class AudienceMovieVO extends BaseMovieVO {
     private Byte status;
     private List<String> joinedPromotions;
--- src/main/java/edu/nju/cinemasystem/data/vo/ManagerMovieVO.java
+++ src/main/java/edu/nju/cinemasystem/data/vo/ManagerMovieVO.java
@@ -4,6 +4,14 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 给经理的电影信息 包含：
+ * releaseDate 上映日期
+ * status 状态：0-未上映 1-热映中 2-已下映 3-已下架
+ * LikeNum 想看人数
+ * likeData 想看数据 是一个list 每一项是一个map key是string
+ *
+ */
 public class ManagerMovieVO extends BaseMovieVO {
     private Date releaseDate;
     private Byte status;
